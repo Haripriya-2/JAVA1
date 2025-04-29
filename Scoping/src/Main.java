d27393e8f3c240c16a66d4e1e2b0ffdd177e19ba
Haripriya Kondra <haripriyakondra2@gmail.com>
2025-04-29 22:51:10 +0530

    Shadowing and Scoping

---
+++ Scoping/src/Main.java
@@ -0,0 +1,51 @@
+public class Main {
+    //Scoping in java
+    //Class Scope:can be accessiable anywhere inside cls.
+    String classLevelVar ="Priya";
+    int x=20;//shadowed by meth scope var.
+    public static void main(String[] args) {
+        //System.out.println(s);//CE:Cant access outside method.
+        // can be accessible only inside same  method
+        new Main().display();
+        int a=10;
+        int b=20;
+
+        //Block Scope:can be acccessible inside block only.
+        {
+            //int a=20; CE:Already defined outside block in same meth.
+            a=20;//reassigns the original val
+            System.out.println("a:"+a);//20
+            int c=30;
+        }
+        System.out.println("a:" +a);//20
+        //System.out.println(c); CE:cant access outside block
+
+        //Scoping in for loop:can be accessiable inside oop only.
+        for(int i=0;i<4;i++){
+            System.out.println("i:"+i);
+            //int a=50;CE:Already defined
+            a=40;
+        }
+        //System.out.println(i);//CE:cant access it outside the loop
+        System.out.println(a);
+
+
+    }
+    //Shadowin:
+    static void shadowing(){
+        int x=30;//shadows cls level var x
+        System.out.println("x: "+x);
+    }
+
+    //cls level
+    void display() {
+        System.out.println(classLevelVar); // accessible
+    }
+
+    // Method Scope:can be accessible inside method only.
+    static void methodScope(){
+        int s=20;
+        System.out.println(s);
+    }
+
+}
